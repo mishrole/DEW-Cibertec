e992d59d8fefedb4795acb4706d5994e9ffa7bed
Mitchell Rodríguez <mitrodle@gmail.com>
2019-06-10 11:53:09 -0500

    Slider - botones específicos | Data - products.json

--- resources/js/main.js
+++ resources/js/main.js
@@ -21,9 +21,56 @@ $(document).ready(function() {
     // Llamada a la función
     slider();
 
-        // Redirección a menu.html al hacer click en 'Pide tu Taipá'
-        $("#menu-list a").eq(3).click(function() {
+    // Path almacena el nombre del archivo en el que nos encontramos
+    var path = (location.pathname).split("/").pop();
+    
+    /* En caso no nos encontremos en la misma página, redirecciona
+       Si estamos en la misma página, mueve el scroll hacia un contenedor */
+    
+    // Inicio > index.html
+    $("#menu-list a").eq(0).click(function() {
+        if(path != "index.html") {
+            $(location).attr('href', 'index.html');
+        }else {
+           $([document.documentElement, document.body]).animate({
+               scrollTop: $("#home").offset().top - 100
+           }, 1000);
+        }
+        return false;
+    });
+
+    // Promociones > #promo
+    $("#menu-list a").eq(1).click(function() {
+        if(path != "index.html") {
+            console.log("No existen los elementos necesarios");
+        }else{
+            $([document.documentElement, document.body]).animate({
+                scrollTop: $("#promo").offset().top - 100
+            }, 1000);
+        }
+        return false;
+    });
+
+    // Taipá del día > #day
+    $("#menu-list a ").eq(2).click(function() {
+        if(path != "index.html") {
+            console.log("No existen los elementos necesarios");
+        }else {
+            $([document.documentElement, document.body]).animate({
+                scrollTop: $("#day").offset().top - 100
+            }, 1000);
+        }
+        return false;
+    });
+    
+    // Pide tu Taipá > menu.html
+    $("#menu-list a").eq(3).click(function() {
+        if(path != "menu.html") {
             $(location).attr('href', 'menu.html');
-            return false;
-        });
+        }else {
+            console.log("Ya estás en la página");
+        }
+        return false;
+    });
+
 });
