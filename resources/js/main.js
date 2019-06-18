4502f4bedc16cafedf1b1cb71691780239669dbe
Mitchell Rodríguez <mitrodle@gmail.com>
2019-06-18 12:14:37 -0500

    #Promo & #Day | Carta en Proceso

--- resources/js/main.js
+++ resources/js/main.js
@@ -28,7 +28,13 @@ function slider() {
     setTimeout("slider()", 2250);
 }
 
-/*      Función de Scroll       */
+var home = $("#home").offset().top
+var promo = $("#promo").offset().top - 50;
+var day = $("#day").offset().top - 25;
+var cart = $("#cart").offset().top - 25;
+var contact = $("#contact").offset().top - 25;
+
+/*      Función de Animación de Scroll al pasar de section     */
 function scrollAnimated(section) {
     $([document.documentElement, document.body]).animate({
         scrollTop: section
@@ -38,38 +44,19 @@ function scrollAnimated(section) {
 /*      Llamada jQuery      */
 $(document).ready(function() {
 
-    // Menu hamburguesa
-    $("#hamburger").click(function(){
-        $(this).toggleClass("hamburger-open");
-
-        if($(this).hasClass("hamburger-open")) {
-            console.log("abierto");
-            $("#menu-options-list").animate({left: "30px"});
-            $("#menu-options").animate({width: "250px"});
-        }else {
-            console.log("cerrado");
-            $("#menu-options-list").animate({left: "-100px"});
-            $("#menu-options").animate({width: "0"});
-        } 
-
-        return false;
-    });
-
     // Llamada a la función Slider
     slider();
 
     /****       Scroll hacia cada sección     ****/ 
     var menuOptionsList = $("#menu-options-list a");
-    var home = $("#home").offset().top
-    var promo = $("#promo").offset().top
-    var day = $("#day").offset().top
-    var cart = $("#cart").offset().top
-    var contact = $("#contact").offset().top
 
    $(".scroll").on("click", function(e) {
-        e.preventDefault();
-        $("html, body").animate({
-            scrollTop: $($(this).attr("href")).offset().top
+       $("#hamburger").removeClass("hamburger-open");
+       $("#menu-options-list").animate({left: "-100px"});
+       $("#menu-options").animate({width: "0"});
+       e.preventDefault();
+       $("html, body").animate({
+        scrollTop: $($(this).attr("href")).offset().top
         }, 900, "linear");
    });
 
@@ -104,6 +91,9 @@ $(document).ready(function() {
     /****     Arrow Scroll Down     ****/
 
     $("#arrow-down-slider").click(function() {
+        $("#hamburger").removeClass("hamburger-open");
+        $("#menu-options-list").animate({left: "-100px"});
+        $("#menu-options").animate({width: "0"});
         $("html, body").animate({
             scrollTop: $("#promo").offset().top
         }, 900, "linear");
@@ -133,8 +123,40 @@ $(document).ready(function() {
         $(this).css("color", "#ffffff");
     })
 
-
     // Uppercase | Título slider-text
-
     $(".slider-title").css("text-transform", "uppercase");
+
+    // Menu hamburguesa
+    $("#hamburger").click(function(){
+        $(this).toggleClass("hamburger-open");
+
+        if($(this).hasClass("hamburger-open")) {
+            console.log("abierto");
+            $("#menu-options-list").animate({left: "30px"});
+            $("#menu-options").animate({width: "250px"});
+        }else {
+            console.log("cerrado");
+            $("#menu-options-list").animate({left: "-100px"});
+            $("#menu-options").animate({width: "0"});
+        } 
+
+        return false;
+    });
+
+});
+
+/*      Efecto aplicado al nav al primer Scroll      */
+// Al pasar de #Home a #Promo, el nav debe cambiar a modo oscuro
+
+$(document).scroll(function() {
+
+    var scrollPos = $(document).scrollTop();
+
+    if(scrollPos >= home && scrollPos < promo) {
+        $("nav").css("background-color", "transparent");
+    }else if(scrollPos >= promo && scrollPos < day) {
+        $("nav").css("background-color", "#0c0c0c");
+    }else if(scrollPos >= day && scrollPos < cart) {
+        $("nav").css("background-color", "#0c0c0c");
+    }
 });
